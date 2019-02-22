3c6f0e0135660b24eb855c63efb78668e30b78a5
araghav22 <abhishek.raghav_cs18@gla.ac.in>
2019-02-22 09:34:45 +0530

    override toString method

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SinglyLinkedList {
 
 
@@ -5,16 +7,32 @@ public class SinglyLinkedList {
     private Node head = null;
     private int size = 0;
 
+    @Override
+    public String toString() {
+
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+
+        Node temp = this.head;
+
+        while(temp.next != null)
+        {
+            result.append(temp.data);
+            temp = temp.next;
+        }
+
+
+        result.append("]");
+
+        return result.toString();
+    }
+
     public static void main(String[] args) {
 
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        System.out.println(linkedList);
     }
 
-
-
-    public SinglyLinkedList(Node head, int size) {
-        this.head = head;
-        this.size = size;
-    }
 
     private static class Node {
 

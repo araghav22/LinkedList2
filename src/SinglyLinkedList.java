84122499e89c19154e23cac887611e74287a8b21
araghav22 <abhishek.raghav_cs18@gla.ac.in>
2019-02-22 09:22:25 +0530

    add variable , head and size

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,5 +1,21 @@
 public class SinglyLinkedList {
 
+
+
+    private Node head = null;
+    private int size = 0;
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public SinglyLinkedList(Node head, int size) {
+        this.head = head;
+        this.size = size;
+    }
+
     private static class Node {
 
         private int data;
@@ -9,6 +25,11 @@ public class SinglyLinkedList {
         private Node(int data, Node next) {
             this.data = data;
             this.next = next;
+        }
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
